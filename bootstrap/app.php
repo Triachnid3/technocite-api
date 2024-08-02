61f041f341cdf7fe8bde35099ba510953c846023
Mattias Cavalier <mattias.cavalier3@gmail.com>
2024-08-02 13:13:22 +0200

    test cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
         $middleware->use([
             \Illuminate\Session\Middleware\StartSession::class,
         ]);
