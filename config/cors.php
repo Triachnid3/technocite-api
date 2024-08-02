19e5e67f4972f7c1292d38d64417e009f7926c18
Mattias Cavalier <mattias.cavalier3@gmail.com>
2024-08-02 11:58:18 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://defi.talents.uwurl.fr'],
 
     'allowed_origins_patterns' => [],
 
